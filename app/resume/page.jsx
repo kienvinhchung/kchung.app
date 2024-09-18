9561b3c69b06941c99f3a6c147762ebef5b8cdee
Kien Chung <kienvchung@gmail.com>
2024-09-18 12:57:47 -0400

    new res

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -350,7 +350,7 @@ const Resume = () => {
                                         {about.title}
                                     </h3>
                                     <Link
-                                    href="https://1drv.ms/w/c/9747102949ee8fa5/EWhOfB95JUNIjApWlm_dHXoBGapOOTvtn9D_Hw0SHyclGQ?e=LtLzFs" 
+                                    href="https://1drv.ms/w/c/9747102949ee8fa5/EWhOfB95JUNIjApWlm_dHXoBGapOOTvtn9D_Hw0SHyclGQ?e=TiphJz" 
                                     target="_blank"
                                     >
                                         <Button 
